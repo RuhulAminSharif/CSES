2f77fc0f7de0cbc9f2374beb4025f963de5c4fb7
Sharif <sharif.cse.ras@gmail.com>
2025-01-08 23:29:08 +0600

    updated static range sum queries

--- 05_Range Queries/01_Static Range Sum Queries.cpp
+++ 05_Range Queries/01_Static Range Sum Queries.cpp
@@ -1,25 +1,18 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define endl "\n"
+#define inf 1e9
 int main()
 {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
-  ll n, q;
-  cin >> n >> q;
-  ll a[n + 5], pref[n + 5] = {0};
-  for (ll i = 1; i <= n; i += 1)
-  {
-    cin >> a[i];
-    pref[i] = pref[i - 1] + a[i];
-  }
-  while (q--)
-  {
-    ll l, r;
-    cin >> l >> r;
-    cout << pref[r] - pref[l - 1] << endl;
-  }
-  return 0;
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ll n, q; cin >> n >> q;
+    vector<ll> v(n+1),pref(n+1,0);
+    for( ll i = 1; i <= n; i += 1 ) cin >> v[i];
+    for( ll i = 1; i <= n; i += 1 ) pref[i] += (pref[i-1]+v[i]);
+    while( q-- ) {
+        ll l, r; cin >> l >> r;
+        cout << pref[r] - pref[l-1] << endl;
+    }
+    return 0;
 }
